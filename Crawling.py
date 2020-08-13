7b9744f004ba64bf855960b09f509ea465dce355
Nonani <cjw1234577@gmail.com>
2020-08-13 09:56:51 +0900

    First Commit

--- Crawling.py
+++ Crawling.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 res = requests.get('https://www.naver.com/')
 # print(res.content)
-
 soup = BeautifulSoup(res.content, 'html.parser')
-
 title = soup.find('title')
 print(title.get_text())
